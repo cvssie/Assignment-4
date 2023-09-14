654899c5a3d7ce63cb3f5cbfe8fea910534e741c
Cassandra K <ckk30@hawaii.edu>
2023-09-13 21:17:54 -1000

    Completed part 5 problem sets
    Finished on your own

--- If else statements pt 1.playground/Contents.swift
+++ If else statements pt 1.playground/Contents.swift
@@ -36,6 +36,59 @@ if gradeA == true
         print("Coding is interesting")
         
     }
+    //part 5 on your own
+// problem 1
+var intOne = 2
+var intTwo = 6
+
+print(intOne > intTwo)
+if intOne == 2
+{
+    print("I like the number 2")
+
+}
     
+else
+{
+    print("and number 6 is pretty cool too")
+}
     
-    
+// problem 2
+var theBest = "This is the best statement"
+var theWorst = " and this is the worst statement"
+
+print (theBest == theBest)
+
+if theBest == "This is the best statement"
+{
+    print(theBest + theWorst)
+}
+
+//Problem 3
+
+var a = true
+var b = false
+
+print(theBest < theWorst)
+if a == true
+{
+    print("Show me the truth")
+
+}
+
+else
+{
+    print("or its false")
+}
+//The end of IF ELSE statements pt 1
+
+
+
+
+
+
+
+
+
+
+
