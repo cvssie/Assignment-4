34b45e7f767341ec765e4e1568d98299122c586c
Cassandra K <ckk30@hawaii.edu>
2023-09-13 20:33:41 -1000

    Part 1-4
    Created If else statements

---
+++ If else statements pt 1.playground/Contents.swift
@@ -0,0 +1,41 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+var goodGrade = "I've been doing all my work"
+
+//create first if statement
+//part 2
+
+if goodGrade == "I've been doing all my homework"
+{
+    //if block
+    //this is the conditional statement
+    print("I should be getting an A at this point")
+    
+    
+}
+    
+//part 3
+else
+
+{
+    //else block
+    print("I need to work harder in this class")
+    
+}
+    //part 4
+var gradeA = true
+if gradeA == true
+{
+    print("I like this class")
+    
+}
+    else
+{
+        print("Coding is interesting")
+        
+    }
+    
+    
+    
